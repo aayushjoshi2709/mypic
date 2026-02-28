7c6ddadc84c7138f56cdc6836a403e5fa1f32e95
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-02-28 23:13:10 +0530

    removed files outside server folder

--- src/user/entity.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	Id       string `bson:"_id,omitempty"`
-	Name     string `bson:"name,omitempty"`
-	UserName string `bson:"username,omitempty"`
-	Password string `bson:"password,omitempty"`
-}
