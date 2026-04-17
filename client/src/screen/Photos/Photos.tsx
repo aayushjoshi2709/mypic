35d01951da119eeb9188cf1476e062baa67f132c
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-04-17 11:14:13 +0530

    fixed columns

--- client/src/screen/Photos/Photos.tsx
+++ client/src/screen/Photos/Photos.tsx
@@ -24,7 +24,7 @@ const Photos = () => {
   }, [dispatch, images]);
   return (
     <div className="flex-1 justify-center w-full">
-      <main className="coluxmns-3 gap-4 p-4 my-4">
+      <main className="columns-3 gap-4 p-4 my-4">
         {images?.map((img) => (
           <Card imgData={img} />
         ))}
